4fb365262c944e5f77af14c9385dba3be52c686f
Rifqi Naufal <116421845+rnaufal52@users.noreply.github.com>
2024-05-09 21:06:43 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,10 @@
 // import modul yang digunakan
 import express from 'express'
-// update
-import path from 'path';
-import { fileURLToPath } from 'url';
-import auth from './middleware/authentication.js'
-// end update
 import * as dotenv from 'dotenv' //untuk env
 import logs from './middleware/logs.js' //
 import authRoute from './route/authRoute.js'
-// import userRoute from './route/userRoute.js'
 import highscoreRoute from './route/highscoreRoute.js'
 import progressRoute from './route/progressRoute.js'
-// import highscorenoauthRoute from './route/highscorenoauthRoute.js'
 
 
 dotenv.config()
@@ -26,20 +19,12 @@ app.use(logs)
 
 // routes
 app.use(authRoute)
-// app.use(userRoute)
 app.use(highscoreRoute)
 app.use(progressRoute)
-// app.use(highscorenoauthRoute)
-
-// akses file .json
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use('/public', auth, express.static(path.join(__dirname, '../public')));
 
 
 
 
-////////////
 app.listen(`${port}`, () => {
     console.log(`Server berjalan di port ${port}`)
 })
