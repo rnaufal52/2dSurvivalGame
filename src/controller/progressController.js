7060a139724823b335d849e8ddf536827cbe0ffd
Rifqi Naufal <116421845+rnaufal52@users.noreply.github.com>
2024-05-09 21:02:10 +0700

    Update progressController.js

--- src/controller/progressController.js
+++ src/controller/progressController.js
@@ -1,13 +1,25 @@
+'use strict'
 import { getProgressModelByToken, UpdateProgressModel } from '../models/progressGameModel.js'
 import * as dotenv from 'dotenv'
-import fs from 'fs'
 import { nanoid } from "nanoid"
+import { Storage } from '@google-cloud/storage'
+import path from 'path';
 
 dotenv.config()
 
+const pathKey = path.resolve('./serviceaccountkey.json')
+
+// cloud storage connect
+const storage = new Storage({
+    projectId: 'apigameastralpursuit',
+    keyFilename: pathKey
+})
+
+// nama bucket
+const bucketName = 'astral-pursuit-progressfile'
+
 // show progress
 const GetProgress = async (req, res) => {
-    const { body } = req
     const user_id = req.user_id
     try {
         const [data] = await getProgressModelByToken(user_id)
@@ -43,13 +55,24 @@ const UpdateProgress = async (req, res) => {
         // Membuat nama file dan path penyimpanan
         const extension = mimetype.split('/')[1];
         const fileName = `${nanoid()}.${extension}`;
-        const filePath = `public/saveFile/${fileName}`;
+        const bucket = storage.bucket(bucketName)
+        const file = bucket.file(fileName)
+
+        // Upload file to bucket
+        await file.save(buffer, {
+            metadata: {
+                contentType: mimetype,
+            },
+        })
+        await file.makePublic()
+
+        const fileUrl = `https://storage.googleapis.com/${bucketName}/${fileName}`
 
         // Mengambil data progress sebelumnya untuk mendapatkan nama file JSON yang akan dihapus
         const [previousProgress] = await getProgressModelByToken(user_id);
 
-        // update file save
-        const [data] = await UpdateProgressModel(user_id, filePath, req.body.chapter)
+        // Update file save
+        const [data] = await UpdateProgressModel(user_id, fileUrl, req.body.chapter)
         if (data.affectedRows === 0) {
             res.status(404).json({
                 code: 404,
@@ -58,10 +81,9 @@ const UpdateProgress = async (req, res) => {
                 data: null,
             })
         } else {
-            // menghapus file yang sudah di upload
-            await handlePreviousFileDeletion(previousProgress);
-            // membuat file baru
-            fs.writeFileSync(filePath, buffer);
+            // Menghapus file sebelumnya jika ada
+            await handlePreviousFileDeletion(previousProgress[0].savefile);
+
             res.json({
                 code: 200,
                 status: "OK",
@@ -79,28 +101,23 @@ const UpdateProgress = async (req, res) => {
     }
 }
 
-const handlePreviousFileDeletion = async (previousProgress) => {
-    if (previousProgress && previousProgress[0] && previousProgress[0].savefile &&
-        previousProgress[0].savefile != "public/savefile/inventorySave.json") {
-        await deletePreviousFile(previousProgress[0].savefile);
-    }
-};
 
-const deletePreviousFile = async (previousFilePath) => {
+const handlePreviousFileDeletion = async (previousFileUrl) => {
     try {
-        // Menambahkan pengecekan eksistensi file sebelum menghapus
-        const isFileExists = await fs.promises.access(previousFilePath, fs.constants.F_OK)
-            .then(() => true)
-            .catch(() => false);
-
-        if (isFileExists) {
-            await fs.promises.unlink(previousFilePath);
-            console.log('File deleted successfully');
-        } else {
-            console.log('File does not exist');
+        // Memeriksa apakah URL file sebelumnya adalah URL yang harus dijaga
+        if (previousFileUrl == 'https://storage.cloud.google.com/astral-pursuit-progressfile/inventorySave.json') {
+            console.log('Previous file is protected and will not be deleted')
+            return
         }
+        // Mendapatkan nama file dari URL
+        const fileName = previousFileUrl.split('/').pop()
+        // Menghapus file dari Google Cloud Storage
+        const bucket = storage.bucket(bucketName)
+        const file = bucket.file(fileName)
+        await file.delete()
+        console.log('File deleted successfully')
     } catch (error) {
-        console.error('Error deleting file:', error);
+        console.error('Error deleting file:', error)
     }
 };
 
